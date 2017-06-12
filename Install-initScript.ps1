59c465014f19b3e5c2ffcc53581c5e8f19590b40
David Bures <david@buresovi.com>
2017-06-12 17:02:40 +0200

    Install-initScript.ps1

--- Install-initScript.ps1
+++ Install-initScript.ps1
@@ -94,7 +94,7 @@ try
     Unzip "c:\OSEL\OSEL.zip" "C:\"    
     LogToFile "runing OSEL init-server.ps1" $logFile    
     Set-Location c:\OSEL\StandAloneScripts\ServerSetup\
-    & C:\OSEL\StandAloneScripts\ServerSetup\init-server.ps1     
+    & .\init-server.ps1     
     LogToFile "OSEL init finished" $logFile
 }
 catch
